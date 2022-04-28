3215d742748e5c1379f38ee54c5e1a12a5bf5276
Nadir Mohamed CHERFIA <m_cherfia@enst.dz>
2022-04-28 01:04:53 +0100

    +

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors=require('cors');
 const mqtt=require('mqtt');
 const { json } = require('express/lib/response');
 
-var sql;
 //const connectWithREtry = () =>{
 ///Create connection 
 const db=mysql.createConnection({
@@ -42,17 +41,16 @@ client.on("error",function(error){
         const jsonFormatedData=JSON.parse(message);
         let sql1='INSERT INTO air_quality SET'+'temperature='+jsonFormatedData.temperature+',humidity='+jsonFormatedData.humidity+',altitude='+jsonFormatedData.altitude+',pressure='+jsonFormatedData.pressure+',PM10='+jsonFormatedData.PM10+',PM25='+jsonFormatedData.PM25+',PM100='+jsonFormatedData.PM100+
         ',P03um='+jsonFormatedData.P03um+',P05um='+jsonFormatedData.P05um+',P10um='+jsonFormatedData.P10um+',P25um='+jsonFormatedData.P25um+',P50um='+jsonFormatedData.P50um+',P100um='+jsonFormatedData.P100um;
-        sql=sql1;
         console.log("message is "+ message);
         console.log("topic is "+ topic1);
+        console.log(sql2)
     });
     client.on('message2',function(topic2, message2, packet){
         const jsonFormatedData2=JSON.parse(message2);
         let sql2=',CO2='+jsonFormatedData2.CO2+',TVOC='+jsonFormatedData2.TVOC+',AIR_QUALITY='+jsonFormatedData2.AIR_QUALITY+',GAS_RESISTANCE='+jsonFormatedData2.GAS_RESISTANCE;
         console.log("message is "+ message2);
         console.log("topic is "+ topic2);
-        let sql=+sql2;
-        console.log(sql)
+        console.log(sql2)
         db.query(sql,(err,result)=>{
             if(err) throw err;
             console.log(result);
