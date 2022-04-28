4a02d10ed4b2a0c0d56b50bdc613f128c4506e03
Nadir Mohamed CHERFIA <m_cherfia@enst.dz>
2022-04-28 01:16:58 +0100

    .

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ client.on("error",function(error){
     client.subscribe(topic2,{qos:1});
     client.on('message',function(topic1, message, packet){
         const jsonFormatedData=JSON.parse(message);
-        let query1='INSERT INTO air_quality SET'+'temperature='+jsonFormatedData.temperature+',humidity='+jsonFormatedData.humidity+',altitude='+jsonFormatedData.altitude+',pressure='+jsonFormatedData.pressure+',PM10='+jsonFormatedData.PM10+',PM25='+jsonFormatedData.PM25+',PM100='+jsonFormatedData.PM100+
+        var query1='INSERT INTO air_quality SET'+'temperature='+jsonFormatedData.temperature+',humidity='+jsonFormatedData.humidity+',altitude='+jsonFormatedData.altitude+',pressure='+jsonFormatedData.pressure+',PM10='+jsonFormatedData.PM10+',PM25='+jsonFormatedData.PM25+',PM100='+jsonFormatedData.PM100+
         ',P03um='+jsonFormatedData.P03um+',P05um='+jsonFormatedData.P05um+',P10um='+jsonFormatedData.P10um+',P25um='+jsonFormatedData.P25um+',P50um='+jsonFormatedData.P50um+',P100um='+jsonFormatedData.P100um;
         console.log("message is "+ message);
         console.log("topic is "+ topic1);
@@ -47,7 +47,7 @@ client.on("error",function(error){
     });
     client.on('message2',function(topic2, message2, packet){
         const jsonFormatedData2=JSON.parse(message2);
-        let query2=',CO2='+jsonFormatedData2.CO2+',TVOC='+jsonFormatedData2.TVOC+',AIR_QUALITY='+jsonFormatedData2.AIR_QUALITY+',GAS_RESISTANCE='+jsonFormatedData2.GAS_RESISTANCE;
+        var query2=',CO2='+jsonFormatedData2.CO2+',TVOC='+jsonFormatedData2.TVOC+',AIR_QUALITY='+jsonFormatedData2.AIR_QUALITY+',GAS_RESISTANCE='+jsonFormatedData2.GAS_RESISTANCE;
         console.log("message is "+ message2);
         console.log("topic is "+ topic2);
         console.log(query2)
