720f530433af643aa5131f9a25442597baa3cf0a
Nadir Mohamed CHERFIA <m_cherfia@enst.dz>
2022-04-28 01:27:24 +0100

    .

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ client.on("error",function(error){
         const jsonFormatedData=JSON.parse(message1);
         var query1='INSERT INTO air_quality SET '+'temperature='+jsonFormatedData.temperature+',humidity='+jsonFormatedData.humidity+',altitude='+jsonFormatedData.altitude+',pressure='+jsonFormatedData.pressure+',PM10='+jsonFormatedData.PM10+',PM25='+jsonFormatedData.PM25+',PM100='+jsonFormatedData.PM100+
         ',P03um='+jsonFormatedData.P03um+',P05um='+jsonFormatedData.P05um+',P10um='+jsonFormatedData.P10um+',P25um='+jsonFormatedData.P25um+',P50um='+jsonFormatedData.P50um+',P100um='+jsonFormatedData.P100um;
-        console.log("message is "+ message1);
-        console.log("topic is "+ topic1);
+        //console.log("message is "+ message1);
+        //console.log("topic is "+ topic1);
         console.log(query1)
     });
     client.on('message2',function(topic2, message2, packet){
@@ -50,7 +50,7 @@ client.on("error",function(error){
         var query2=",CO2="+jsonFormatedData2.CO2+",TVOC="+jsonFormatedData2.TVOC+",AIR_QUALITY="+jsonFormatedData2.AIR_QUALITY+",GAS_RESISTANCE="+jsonFormatedData2.GAS_RESISTANCE;
         //console.log("message is "+ message2);
         //console.log("topic is "+ topic2);
-        console.log("2:"+query2)
+        console.log(query2)
         db.query(query2,(err,result)=>{
             if(err) throw err;
             console.log(result);
